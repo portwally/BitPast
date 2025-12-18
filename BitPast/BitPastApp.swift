7f23ceb11a0c321bb568e8f68960fce71fd7a64b
portwally <walter.tengler@gmail.com>
2025-12-18 08:44:20 +0000

    Initial Commit

---
+++ BitPast/BitPastApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitPastApp.swift
+//  BitPast
+//
+//  Created by Walter Tengler on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BitPastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitPastTests/BitPastTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitPastTests.swift
+//  BitPastTests
+//
+//  Created by Walter Tengler on 18/12/2025.
+//
+
+import Testing
+@testable import BitPast
+
+struct BitPastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
